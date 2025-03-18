e07698d4a5596515a28f730a6eea76b494588ddf
gopaldhanve2003 <143928521+gopaldhanve2003@users.noreply.github.com>
2025-03-18 21:22:12 +0530

    forgot to ignore ota zip ;p

--- crave_build.sh
+++ crave_build.sh
@@ -646,7 +646,7 @@ set -v
 #######################################
 
 # Search for the generated ZIP file based on Device (latest file by modification time)
-ZIP_FILE=$(find out/target/product/"${DEVICE}" -maxdepth 1 -type f -iname "*${DEVICE}*.zip" -printf '%T@ %p\n' 2>/dev/null | sort -n | tail -n 1 | cut -d' ' -f2-)
+ZIP_FILE=$(find out/target/product/"${DEVICE}" -maxdepth 1 -type f -iname "*${DEVICE}*.zip" ! -iname "*ota*.zip" -printf '%T@ %p\n' 2>/dev/null | sort -n | tail -n 1 | cut -d' ' -f2-)
 if [ -z "$ZIP_FILE" ]; then
     failStage "No ZIP file found for device ${DEVICE}"
 fi
